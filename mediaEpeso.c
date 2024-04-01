53866dd1d1f4d82379ebb302ec7395b3e2a621eb
Rodrigo Victor Beltrão <150438774+Rodibre8708@users.noreply.github.com>
2024-04-01 08:32:13 -0300

    Create mediaEpeso.c

---
+++ mediaEpeso.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void calcularMedia(double notas[][colunas], double pesos[colunas], int qtdAlunos, int qtdNotas);
+
+int main() {
+
+notas1 = (n1 + p11 + p21 + pro1)
+peso = [1, 2, 2, 3]
+
+printf("Nota teste: ");
+scanf("%f", &n1);
+printf("Nota prova 1: ");
+scanf("%f", &p11);
+printf("Nota prova 2: ");
+scanf("%f", &p21);
+printf("Nota projeto: ");
+scanf("%f", &pro1);
+
+
+void calcularMedia(double notas1[][colunas], double pesos[colunas], int qtdNotas){
+    for(int i = 0)
+    
+    
+    
+}
+    
+    
+
+    return 0;
+}
